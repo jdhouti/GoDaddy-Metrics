95197a928fc498ee9eb176d9661eeb328f01ed10
Julien Dhouti <julien.dhouti@gmail.com>
2018-08-01 18:49:12 -0700

    added peak arrows to show peak times during the day

--- queue/graph.py
+++ queue/graph.py
@@ -4,52 +4,70 @@ import pandas as pd
 import plotly
 import datetime
 import numpy as np
+import sys
 
-data_filename = 'queue_data_' + str(datetime.datetime.now().date()) + '_test.csv'
-
-# Create the dataframe
-def create_graph(frame):
-	"""Returns a go.Figure object."""
-	df = pd.read_csv(frame)
-	df.columns = ['Inbound', 'Agents', 'French', 'time']
-	df_colors = ['hsl('+str(h)+',50%'+',50%)' for h in np.linspace(0, 360, df.columns.size)]
-	df['time'] = pd.to_datetime(df['time'])
-	data = []
-
-	# Create traces
-	for i in df.columns[:-1]:
-		data.append(
-			go.Scatter(
-				x=df['time'],
-				y=df[i],
-				mode='lines',
-				marker=dict(
-					color=df_colors[list(df.columns).index(i)]
-				),
-				name=i
-			)
-		)
-	# Edit the layout
-	layout = go.Layout(
-		title='Queue Report for ' + str(datetime.datetime.now().date()),
-		xaxis=dict(
-			title='TIME'
-		),
-		yaxis=dict(
-			title='NUMBER OF PEOPLE'
-		)
-	)
-	return go.Figure(data=data, layout=layout)
+data_filename = 'queue_data_' + str(datetime.datetime.now().date()) + '.csv'
+print('info : generating report for ' + data_filename)
 
 try:
-    print('info : generating report for ' + data_filename)
-    #create the graph
-    final_graph = create_graph(data_filename)
-
-    # plot the graph and save the graph
-    image_name = str(datetime.datetime.now().date()) + '_report'
-    plotly.offline.plot(final_graph, filename=(image_name + '.html'))
-    print('info : report generated.')
+	df = pd.read_csv(data_filename)
+	df.columns = ['Inbound', 'Agents', 'French', 'time']
+	df['time'] = pd.to_datetime(df['time'])
 except:
-    print('error: could not generate report :(')
-    input()
+	print('error: could not generate report :(')
+	input()
+	sys.exit()
+
+df_colors = ['hsl('+str(h)+',50%'+',50%)' for h in np.linspace(0, 360, df.columns.size)]
+data, annotations = [], []
+
+# Create traces
+for i in df.columns[:-1]:
+	data.append(
+		go.Scatter(
+			x=df['time'],
+			y=df[i],
+			mode='lines',
+			marker=dict(
+				color=df_colors[list(df.columns).index(i)]
+			),
+			name=i
+		)
+	)
+
+for i in df.columns[:-1]:
+	max_index = df[i].idxmax
+	annotations.append(
+		dict(
+			x=df['time'][max_index],
+			y=df[i][max_index],
+			xref='x',
+			yref='y',
+			text='peak: ' + str(df[i][max_index]),
+			showarrow=True,
+			arrowhead=1,
+			ax=0,
+			ay=-40
+		)
+	)
+
+# Edit the layout
+layout = go.Layout(
+	annotations=annotations,
+	title='Queue Report for ' + str(datetime.datetime.now().date()),
+	xaxis=dict(
+		title='TIME',
+		zeroline=True,
+	),
+	yaxis=dict(
+		showline=True,
+		zeroline=True,
+		title='NUMBER OF PEOPLE',
+	)
+)
+
+figure = go.Figure(layout=layout, data=data)
+
+image_name = str(datetime.datetime.now().date()) + '_report'
+plotly.offline.plot(figure, filename=(image_name + '.html'))
+print('info : report generated.')
